c644a8f7dbee9cce6f1b749217c79eb7a5b31784
COLLINS OMWOYO <collinsomwoyo@gmail.com>
2024-09-12 21:54:57 +0300

    "Added 0-add.py script that imports add function from add_0 and uses it to print the sum of two variables."

---
+++ 0x02-python-import_modules/0-add.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+# Importing the add function hopefully
+from add_0 import add
+
+# Variables
+a = 1
+b = 2
+
+# Printing the result
+print("{} + {} = {}".format(a, b, add(a, b)))
