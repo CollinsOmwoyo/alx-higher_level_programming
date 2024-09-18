a46f4a81f76392732fea6d2f6af4b40df9da9c2a
COLLINS OMWOYO <collinsomwoyo@gmail.com>
2024-09-18 22:54:57 +0300

    "Removed indentation from print statement and main function call."

--- 0x02-python-import_modules/0-add.py
+++ 0x02-python-import_modules/0-add.py
@@ -7,7 +7,7 @@ a = 1
 b = 2
 
 # print function with string formatting
-    print("{} + {} = {}".format(a, b, add(a, b)))
+print("{} + {} = {}".format(a, b, add(a, b)))
 
 if __name__ == "__main__":
-    main()
+main()
