2a9d65d427667c2b40c524dd1c68f87558b8c41a
COLLINS OMWOYO <collinsomwoyo@gmail.com>
2024-09-18 22:41:43 +0300

    "Updated example usage of add function in 0-add.py"

--- 0x02-python-import_modules/0-add.py
+++ 0x02-python-import_modules/0-add.py
@@ -11,5 +11,5 @@ return a + b
 if __name__ == "__main__":
 # Example usage of the add function
 print("This module is being run directly")
-result = add(3, 4)
-print("3 + 4 = {}".format(result))
+result = add(1, 2)
+print("1 + 2 = {}".format(result))
