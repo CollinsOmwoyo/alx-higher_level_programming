8609a830fcec2e9cc464619ac458d0f69168540d
COLLINS OMWOYO <collinsomwoyo@gmail.com>
2024-09-19 16:14:01 +0300

    "Simplify 0-run.sh script by removing environment variable and file existence checks."

--- 0x00-python-hello_world/0-run.sh
+++ 0x00-python-hello_world/0-run.sh
@@ -1,15 +1,2 @@
 #!/bin/bash
-# Check if the PYFILE environment variable is set
-if [ -z "$PYFILE" ]; then
-echo "Error: PYFILE environment variable is not set."
-exit 1
-fi
-
-# Check if the specified file exists and is executable
-if [ ! -f "$PYFILE" ]; then
-echo "Error: The file specified in PYFILE does not exist."
-exit 1
-fi
-
-# Run the Python script
-python3 "$PYFILE"
+python3 $PYFILE
