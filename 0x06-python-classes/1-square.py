063631edcab33e5bac0147d701c07915e5033a85
COLLINS OMWOYO <collinsomwoyo@gmail.com>
2024-10-03 00:48:01 +0300

    "Added an empty Square class with a docstring."

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+"""
+This module defines an empty class Square.
+Yes, nothing fancy here, just an empty shell for squares.
+"""
+class Square:
+    """A simple class that defines a square. OOP!"""
+    pass
