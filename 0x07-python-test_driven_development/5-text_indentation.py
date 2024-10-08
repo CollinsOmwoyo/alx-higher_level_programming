f84d14f04d7b160164f5ee33d46b1559d2908184
COLLINS OMWOYO <collinsomwoyo@gmail.com>
2024-10-08 22:55:47 +0300

    Added function say_my_name with example usage and error handling for non-string input.

---
+++ 0x07-python-test_driven_development/5-text_indentation.py
@@ -0,0 +1,10 @@
+>>> say_my_name("Omwoyo", "Ogega")
+My name is Omwoyo Ogega
+>>> say_my_name("Walter", "White")
+My name is Walter White
+>>> say_my_name("Janet")
+My name is Janet
+>>> say_my_name(12, "White")
+Traceback (most recent call last):
+    ...
+TypeError: first_name must be a string
