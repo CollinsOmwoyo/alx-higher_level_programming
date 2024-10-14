a229b93b694930679588dd1b10c7683d893606b8
COLLINS OMWOYO <collinsomwoyo@gmail.com>
2024-10-15 02:06:54 +0300

    "Adds BaseGeometry class with area method that raises an exception."

---
+++ 0x0A-python-inheritance/6-base_geometry.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python3
+class BaseGeometry:
+    """Defines geometry operations."""
+
+    def area(self):
+        """Raises an exception since area is not implemented."""
+        raise Exception("area() is not implemented")
