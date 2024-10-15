652ac808c15010a3351e8d060f45ea6fc954f84b
COLLINS OMWOYO <collinsomwoyo@gmail.com>
2024-10-15 23:22:06 +0300

    "Added docstring to Rectangle class and its methods, and updated comments in __init__ method."

--- 0x0A-python-inheritance/8-rectangle.py
+++ 0x0A-python-inheritance/8-rectangle.py
@@ -1,11 +1,27 @@
 #!/usr/bin/python3
+"""
+This module defines the `Rectangle` class, from `BaseGeometry`
+and represents a rectangle with validated dimensions.
+"""
+
+
 BaseGeometry = __import__('7-base_geometry').BaseGeometry
 
 class Rectangle(BaseGeometry):
-    """Represents a rectangle."""
+    """A class to represent a rectangle, from BaseGeometry."""
 
     def __init__(self, width, height):
-        """Initializes width and height with validation."""
+        """
+        Initialize the rectangle with width and height.
+
+        Args:
+            width (int): The width of the rectangle.
+            height (int): The height of the rectangle.
+
+        Raises:
+            TypeError: If width or height is not an integer.
+            ValueError: If is less than or equal to 0.
+        """
         self.integer_validator("width", width)
         self.__width = width
         self.integer_validator("height", height)
