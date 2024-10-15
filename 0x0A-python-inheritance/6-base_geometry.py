62ab87bec965e787695967a40f013f7097ec132e
COLLINS OMWOYO <collinsomwoyo@gmail.com>
2024-10-15 23:16:54 +0300

    "Added docstring to BaseGeometry class and its methods, and reformatted comments."

--- 0x0A-python-inheritance/6-base_geometry.py
+++ 0x0A-python-inheritance/6-base_geometry.py
@@ -1,7 +1,18 @@
 #!/usr/bin/python3
+"""
+This module defines the `BaseGeometry` class, a template
+for geometry-related operations.
+"""
+
+
 class BaseGeometry:
-    """Defines geometry operations."""
+    """A class for basic geometry operations."""
 
     def area(self):
-        """Raises an exception since area is not implemented."""
+        """
+        Raise an exception to indicate that the area method isn't.
+
+        Raises:
+            Exception: Always raised with the message 'area() isn't'.
+        """
         raise Exception("area() is not implemented")
