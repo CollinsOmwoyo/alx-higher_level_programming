fcc7a31e3b217e70835328b268851475f9701e19
COLLINS OMWOYO <collinsomwoyo@gmail.com>
2024-10-15 23:19:18 +0300

    "Added docstrings to BaseGeometry class and its methods."

--- 0x0A-python-inheritance/7-base_geometry.py
+++ 0x0A-python-inheritance/7-base_geometry.py
@@ -1,13 +1,34 @@
 #!/usr/bin/python3
+"""
+This module defines the `BaseGeometry` class, basic
+geometry-related operations and validation methods.
+"""
+
+
 class BaseGeometry:
-    """Defines geometry operations."""
+    """A class for basic geometry operations and value validation."""
 
     def area(self):
-        """Raises an exception since area is not implemented."""
+        """
+        Raise an exception to indicate that the area method isn't.
+
+        Raises:
+            Exception: Always raised with the message 'area() isn't'.
+        """
         raise Exception("area() is not implemented")
 
     def integer_validator(self, name, value):
-        """Validates if value is a positive integer."""
+        """
+        Validate that a value is a positive integer.
+
+        Args:
+            name (str): Name of the parameter being validated.
+            value (int): The value to validate.
+
+        Raises:
+            TypeError: If the value is not an integer.
+            ValueError: If the value is not greater than 0.
+        """
         if not isinstance(value, int):
             raise TypeError(f"{name} must be an integer")
         if value <= 0:
