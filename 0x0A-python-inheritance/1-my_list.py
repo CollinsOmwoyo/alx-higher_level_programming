90d94795ce0f9113dd81ed2976ce404608cbb9c0
COLLINS OMWOYO <collinsomwoyo@gmail.com>
2024-10-15 09:26:04 +0300

    "Updated docstrings in MyList class and module"

--- 0x0A-python-inheritance/1-my_list.py
+++ 0x0A-python-inheritance/1-my_list.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
+"""Module containing MyList class that extends list."""
 class MyList(list):
-    """Inherits list and implements a sorted print function."""
+    """Class that inherits list and implements a sorted print function."""
     
     def print_sorted(self):
         """Prints the list in ascending order."""
