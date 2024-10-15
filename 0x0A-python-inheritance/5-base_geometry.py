226481327390edcc2bdf38d3924b27c3442504db
COLLINS OMWOYO <collinsomwoyo@gmail.com>
2024-10-15 23:10:37 +0300

    "Added a blank line between the module docstring and the class definition in BaseGeometry.py"

--- 0x0A-python-inheritance/5-base_geometry.py
+++ 0x0A-python-inheritance/5-base_geometry.py
@@ -4,6 +4,7 @@ This module defines the `BaseGeometry` class, which serves as a base for
 geometric operations.
 """
 
+
 class BaseGeometry:
     """An empty class that serves as a foundation for geometry-related objects."""
     pass
