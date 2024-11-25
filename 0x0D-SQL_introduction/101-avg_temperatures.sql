7438cb6b79630cf3f7314c4943ccb00616f7830e
COLLINS OMWOYO <collinsomwoyo@gmail.com>
2024-11-25 21:11:18 +0300

    "Added comments to SQL script to describe its purpose and functionality"

--- 0x0D-SQL_introduction/101-avg_temperatures.sql
+++ 0x0D-SQL_introduction/101-avg_temperatures.sql
@@ -1,4 +1,6 @@
--- 101-avg_temperatures.sql
+-- Script to calculate the average temperature (Fahrenheit) by city
+-- Results are ordered by temperature in descending order
+
 SELECT city, AVG(temperature) AS avg_temp
 FROM temperatures
 GROUP BY city
