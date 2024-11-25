21b173d9063687af1c9d51540903e7afb2212811
COLLINS OMWOYO <collinsomwoyo@gmail.com>
2024-11-25 21:10:36 +0300

    "Replaced SQL query with script to convert database and table to UTF8"

--- 0x0D-SQL_introduction/100-move_to_utf8.sql
+++ 0x0D-SQL_introduction/100-move_to_utf8.sql
@@ -1,7 +1,15 @@
--- Script to calculate the average temperature (Fahrenheit) by city
--- Results are ordered by temperature in descending order
+-- Script to convert the database hbtn_0c_0 and its components to UTF8 (utf8mb4, utf8mb4_unicode_ci)
+-- This includes the database, table first_table, and field name in first_table
 
-SELECT city, AVG(temperature) AS avg_temp
-FROM temperatures
-GROUP BY city
-ORDER BY avg_temp DESC;
+-- Convert the database to UTF8
+ALTER DATABASE hbtn_0c_0 CHARACTER SET = utf8mb4 COLLATE = utf8mb4_unicode_ci;
+
+-- Use the database to ensure subsequent commands apply
+USE hbtn_0c_0;
+
+-- Convert the table first_table to UTF8
+ALTER TABLE first_table CONVERT TO CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
+
+-- Convert the name field in first_table to UTF8
+ALTER TABLE first_table
+MODIFY name VARCHAR(256) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
